489191590240cbd87526b12989d98c6c04b74baa
Hayden Toupin <h.e.toupin@gmail.com>
2025-07-26 14:55:35 -0700

    Main code

---
+++ source/main.py
@@ -0,0 +1,21 @@
+from menu import Menu
+from rotary import Encoder
+from ssd1306 import SSD1306_SPI
+from fm_radio import Radio
+from machine import Pin, Timer
+import utime
+
+volume_encoder = Encoder(18, 17)
+selection_encoder = Encoder(27, 26)
+
+select_button = Pin(28, Pin.IN, Pin.PULL_UP)
+snooze_button = Pin(16, Pin.IN, Pin.PULL_UP)
+
+radio = Radio(101.9, 2, False)
+
+menu = Menu(radio, volume_encoder, selection_encoder, select_button, snooze_button)
+
+while True: 
+    utime.sleep_ms(1)
+    menu.update()
+
