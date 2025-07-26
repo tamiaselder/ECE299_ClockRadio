423e78a269781f13e0a3bab0b6bf39ef0e3db212
Hayden Toupin <h.e.toupin@gmail.com>
2025-07-26 16:15:21 -0700

    Added Radio Functionality to Menu

--- source/menu.py
+++ source/menu.py
@@ -18,7 +18,7 @@ class Screens:
 
 class Menu():
     def __init__(self, radio: Radio, vol_encoder: Encoder, selection_endcoder: Encoder, selection_button: Pin, reset_button: Pin):
-        self.radio = radio
+        self._radio = radio
 
         self._current_screen = Screens.STANDBY
         self._current_option = 0
@@ -44,11 +44,11 @@ class Menu():
         self._reset_button_timer = Timer()
         self._snooze_timer = Timer()
 
-        self._vol_encoder.set_update_val(self.radio.GetSettings()[2], 15)
-        self._selection_encoder.set_update_val(0, 2)
+        self._vol_encoder.set_update_val(self._radio.GetSettings()[2], 0, 15)
+        self._selection_encoder.set_update_val(0, 0, 2)
 
-        self._selection_button.irq(self._selection_button_callback(), Pin.IRQ_RISING)
-        self._reset_button.irq(self._reset_button_callback(), Pin.IRQ_RISING)
+        self._selection_button.irq(self._selection_button_callback, Pin.IRQ_RISING)
+        self._reset_button.irq(self._reset_button_callback, Pin.IRQ_RISING)
         
         self._previous_value = 0
 
@@ -65,37 +65,35 @@ class Menu():
         #     self.radio.UpdateSettings
         
         value = self._selection_encoder.value()
-
-        if(not self._in_screen):
-            self._current_screen = value
-
-        if(self._in_screen and not self._in_option):
-            if(self._current_screen == Screens.TIME_MENU):
-                self._current_option = value
-            elif(self._current_screen == Screens.RADIO_MENU):
-                pass
-            elif(self._current_screen == Screens.STANDBY):
-                pass
-        
-        if(self._in_option):
-            if(self._current_option == ClockSettings.TIME_HOUR):
-                time = self.get_time()
-                date = self.get_date()
-                self._rtc.datetime([date[0], date[1], date[2], date[3], value, time[1], time[2], time[3]])
-            elif(self._current_option == ClockSettings.TIME_MIN):
-                time = self.get_time()
-                date = self.get_date()
-                self._rtc.datetime([date[0], date[1], date[2], date[3], time[0], value, time[2], time[3]])
-            elif(self._current_option == ClockSettings.TIME_FORMAT):
-                self._time_format_24 = value
-            elif(self._current_option == ClockSettings.ALARM_SET):
-                self._alarm_set = value
-            elif(self._current_option == ClockSettings.ALARM_MIN):
-                self._alarm_time[1] = value
-            elif(self._current_option == ClockSettings.ALARM_HOUR):
-                self._alarm_time[0] = value
-        
         if(value != self._previous_value):
+            if(not self._in_screen):
+                self._current_screen = value
+
+            if(self._in_screen and not self._in_option):
+                if(self._current_screen == Screens.TIME_MENU):
+                    self._current_option = value
+                elif(self._current_screen == Screens.RADIO_MENU):
+                    self._radio.SetFrequency(value)
+                    self._radio.ProgramRadio()
+                elif(self._current_screen == Screens.STANDBY):
+                    pass
+            
+            if(self._in_option):
+                if(self._current_option == ClockSettings.TIME_HOUR):
+                    self.set_time_hour(value)
+                elif(self._current_option == ClockSettings.TIME_MIN):
+                    self.set_time_minute(value)
+                elif(self._current_option == ClockSettings.TIME_FORMAT):
+                    self._time_format_24 = value
+                elif(self._current_option == ClockSettings.ALARM_SET):
+                    self._alarm_set = value
+                elif(self._current_option == ClockSettings.ALARM_MIN):
+                    self._alarm_time[1] = value
+                elif(self._current_option == ClockSettings.ALARM_HOUR):
+                    self._alarm_time[0] = value
+            
+            print("Current Station")
+            print(self.get_station())
             print("Current Screen") 
             print(self.get_screen())
             print("Current Option") 
@@ -109,42 +107,51 @@ class Menu():
         
         self._previous_value = value
 
-    def _reset_button_callback(self):
-        self._reset_button_timer.init(mode=Timer.ONE_SHOT, period=10, callback=self._reset_button_timer_callback())
+    def _reset_button_callback(self,pin):
+        self._reset_button_timer.init(mode=Timer.ONE_SHOT, period=10, callback=self._reset_button_timer_callback)
 
-    def _reset_button_timer_callback(self):
-        pass
+    def _reset_button_timer_callback(self,pin):
+        if(self._in_screen and not self._in_option):
+            self._in_screen == False
 
-    def _selection_button_callback(self):
-        self._reset_button_timer.init(mode=Timer.ONE_SHOT, period=10, callback=self._selection_button_timer_callback())
+    def _selection_button_callback(self,pin):
+        self._reset_button_timer.init(mode=Timer.ONE_SHOT, period=10, callback=self._selection_button_timer_callback)
 
-    def _selection_button_timer_callback(self):
-        print("yes")
+    def _selection_button_timer_callback(self,pin):
         if(not self._in_option and self._in_screen and self._current_screen == Screens.TIME_MENU):
             self._in_option = True
             if(self._current_option == ClockSettings.TIME_HOUR):
-                self._selection_encoder.set_update_val(self.get_time()[0], 23)
+                self._selection_encoder.set_update_val(self.get_time()[0], 0, 23)
+                self._selection_encoder.set_update_increment(1)
             elif(self._current_option == ClockSettings.TIME_MIN):
-                self._selection_encoder.set_update_val(self.get_time()[1], 59)
+                self._selection_encoder.set_update_val(self.get_time()[1], 0, 59)
+                self._selection_encoder.set_update_increment(1)
             elif(self._current_option == ClockSettings.TIME_FORMAT):
-                self._selection_encoder.set_update_val(self._time_format_24, 1)
+                self._selection_encoder.set_update_val(self._time_format_24, 0, 1)
+                self._selection_encoder.set_update_increment(1)
             elif(self._current_option == ClockSettings.ALARM_SET):
                 self._selection_encoder.set_update_val(self._alarm_set, 1)
+                self._selection_encoder.set_update_increment(1)
             elif(self._current_option == ClockSettings.ALARM_MIN):
-                self._selection_encoder.set_update_val(self._alarm_time[1], 59)
+                self._selection_encoder.set_update_val(self._alarm_time[1], 0, 59)
+                self._selection_encoder.set_update_increment(1)
             elif(self._current_option == ClockSettings.ALARM_HOUR):
-                self._selection_encoder.set_update_val(self._alarm_time[0], 24)
+                self._selection_encoder.set_update_val(self._alarm_time[0], 0, 23)
+                self._selection_encoder.set_update_increment(1)
         
         elif(self._in_option):
             self._in_option = False
-            self._selection_encoder.set_update_val(self._current_option, 5)
+            self._selection_encoder.set_update_val(self._current_option, 0, 5)
+            self._selection_encoder.set_update_increment(1)
 
         elif(not self._in_screen):
             self._in_screen = True
             if(self._current_screen == Screens.TIME_MENU):
-                self._selection_encoder.set_update_val(self._current_option, 5)
+                self._selection_encoder.set_update_val(self._current_option, 0, 5)
+                self._selection_encoder.set_update_increment(1)
             elif(self._current_screen == Screens.RADIO_MENU):
-                pass
+                self._selection_encoder.set_update_val(self.get_station(), 87, 108)
+                self._selection_encoder.set_update_increment(0.1)
             elif(self._current_screen == Screens.STANDBY):
                 pass
 
@@ -157,13 +164,23 @@ class Menu():
         return self._current_option
     
     def get_station(self):
-        pass
+        return self._radio.GetSettings()[2]
 
     def get_time(self):
         time = self._rtc.datetime()
         if(not self._time_format_24 and time[4] > 12):
             return (time[4] - 12, time[5], time[6], time[7])
         return (time[4], time[5], time[6], time[7])
+    
+    def set_time_hour(self, hour):
+        time = self.get_time()
+        date = self.get_date()
+        self._rtc.datetime([date[0], date[1], date[2], date [3], hour, time[1], time[2], time[3]])
+    
+    def set_time_minute(self, minute):
+        time = self.get_time()
+        date = self.get_date()
+        self._rtc.datetime([date[0], date[1], date[2], date [3], time[0], minute, time[2], time[3]])
     
     def get_date(self):
         date = self._rtc.datetime()
